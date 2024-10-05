91f4672a047297478c5c98d2ee870270ca8153f0
Your Name <your.email@example.com>
2024-10-04 23:59:34 -0500

    /

--- src/router/PromocionRouter.ts
+++ src/router/PromocionRouter.ts
@@ -3,6 +3,9 @@ import { getProm, getPromociones, getPromocionPagoById } from '../controllers/Pr
 
 export default (router: express.Router) => {
     // Define a route with a path parameter ':id' and call getPromocionDias
+    router.get('/', (req: express.Request, res: express.Response) => {
+        res.status(200).json({ Status: 'UP' });
+    });
     router.get('/promociones', getPromociones);
     router.get('/promocion/:id', getProm);
     router.get('/promocion/:id/pago', getPromocionPagoById)
