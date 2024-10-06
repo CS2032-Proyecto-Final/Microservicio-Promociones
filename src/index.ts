6ababc9905be6b5ffc7eb547e6866bc6c9c9b43a
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2024-10-05 23:23:53 -0500

    ahora usa la ip privada de la mv de base de datos

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ server.listen(8002, () => {
     console.log('Server running on http://localhost:8002/')
 })
 
-const MONGO_URL = "mongodb://34.202.67.244:8002/promociones_db"
+const MONGO_URL = "mongodb://172.31.39.248:8002/promociones_db"
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
